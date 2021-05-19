23511a6145f41a38abf261346e86b8730b24262c
wxsl5 <wxsl1997@163.com>
2021-05-19 22:42:08 +0800

    v1.0.0 offer36 二叉搜索树与双向链表

---
+++ offer/src/main/java/org/wxsl/leetcode/offer/m2105/w3/Offer36.java
@@ -0,0 +1,48 @@
+package org.wxsl.leetcode.offer.m2105.w3;
+
+/**
+ * 二叉搜索树与双向链表
+ */
+public class Offer36 {
+
+    /**
+     * 思路:递归left和right, 然后与root合并即可
+     */
+    public static Node treeToDoublyList(Node root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        //接左边,并确认最左边的节点
+        Node leftHead = treeToDoublyList(root.left);
+        Node leftestNode = leftHead == null ? root : leftHead;
+        if (leftHead != null) {
+            root.left = leftHead.left;
+            leftHead.left.right = root;
+        }
+
+        //接右边,并确认最右边的节点
+        Node rightHead = treeToDoublyList(root.right);
+        Node rightestNode = rightHead == null ? root : rightHead.left;
+        if (rightHead != null) {
+            root.right = rightHead;
+            rightHead.left = root;
+        }
+
+        //循环链表
+        leftestNode.left = rightestNode;
+        rightestNode.right = leftestNode;
+        return leftestNode;
+    }
+
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+}
---
+++ offer/src/test/java/org/wxsl/leetcode/offer/m2105/w3/Offer36Test.java
@@ -0,0 +1,32 @@
+package org.wxsl.leetcode.offer.m2105.w3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Offer36Test {
+
+    @Test
+    void treeToDoublyList() {
+        Offer36.Node node1 = new Offer36.Node(1);
+        Offer36.Node node2 = new Offer36.Node(2);
+        Offer36.Node node3 = new Offer36.Node(3);
+        Offer36.Node node4 = new Offer36.Node(4);
+        Offer36.Node node5 = new Offer36.Node(5);
+
+        node4.left = node2;
+        node4.right = node5;
+
+        node2.left = node1;
+        node1.right = node3;
+
+        Offer36.Node headNode = Offer36.treeToDoublyList(node4);
+
+        // 最左节点
+        Assertions.assertSame(node1, headNode);
+
+        // 最右节点
+        Assertions.assertSame(headNode.left, node5);
+    }
+}
